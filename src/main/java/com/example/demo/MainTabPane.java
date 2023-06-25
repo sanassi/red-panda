557d0b0d9a4e76ffc2db78ef1aaafd20c05c6e60
Sanassi Cissé <sanassi-mory.cisse@epita.fr>
2023-06-25 22:42:32 +0200

    feat: added richtextFX to project, to perfrom basic syntax highlighting, display line numbers"
    Signed-off-by: Sanassi Cissé <sanassi-mory.cisse@epita.fr>

--- src/main/java/com/example/demo/MainTabPane.java
+++ src/main/java/com/example/demo/MainTabPane.java
@@ -11,14 +11,18 @@ import org.fxmisc.richtext.LineNumberFactory;
 import org.fxmisc.richtext.model.Paragraph;
 import org.fxmisc.richtext.model.StyleSpans;
 import org.fxmisc.richtext.model.StyleSpansBuilder;
+import org.reactfx.Subscription;
 import org.reactfx.collection.ListModification;
 
+import java.time.Duration;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static com.example.demo.SyntaxHighlighting.computeHighlighting;
 
 
 public class MainTabPane extends TabPane {
@@ -42,26 +46,45 @@ public class MainTabPane extends TabPane {
         addTab.setClosable(false);
         this.getSelectionModel().selectedItemProperty().addListener((observable, oldTab, newTab) -> {
             if(newTab == addTab) {
-                this.getTabs().add(this.getTabs().size() - 1, CreateTabWithTextArea("untitled")); // Adding new tab before the "button" tab
+                this.getTabs().add(this.getTabs().size() - 1, CreateTabWithCodeArea("untitled")); // Adding new tab before the "button" tab
                 this.getSelectionModel().select(this.getTabs().size() - 2); // Selecting the tab before the button, which is the newly created one
             }
         });
         return addTab;
     }
 
-    public Tab CreateTabWithTextArea(String tabTitle) {
+    public Tab CreateTabWithCodeArea(String tabTitle) {
         Tab tab = new Tab(tabTitle);
         tab.setClosable(true);
-        //TextArea textArea = new TextArea();
-        //tab.setContent(textArea);
 
         CodeArea codeArea = new CodeArea();
         codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
         codeArea.setContextMenu( new DefaultContextMenu() );
 
+
+        // recompute the syntax highlighting for all text, 500 ms after user stops editing area
+        // Note that this shows how it can be done but is not recommended for production with
+        // large files as it does a full scan of ALL the text every time there is a change !
+        Subscription cleanupWhenNoLongerNeedIt = codeArea
+
+                // plain changes = ignore style changes that are emitted when syntax highlighting is reapplied
+                // multi plain changes = save computation by not rerunning the code multiple times
+                //   when making multiple changes (e.g. renaming a method at multiple parts in file)
+                .multiPlainChanges()
+
+                // do not emit an event until 500 ms have passed since the last emission of previous stream
+                .successionEnds(Duration.ofMillis(500))
+
+                // run the following code block when previous stream emits an event
+                .subscribe(ignore -> codeArea.setStyleSpans(0, computeHighlighting(codeArea.getText())));
+
+        // when no longer need syntax highlighting and wish to clean up memory leaks
+        // run: `cleanupWhenNoLongerNeedIt.unsubscribe();`
+
+
         codeArea.getVisibleParagraphs().addModificationObserver
                 (
-                        new VisibleParagraphStyler<>( codeArea, this::computeHighlighting )
+                        new SyntaxHighlighting.VisibleParagraphStyler<>( codeArea, SyntaxHighlighting::computeHighlighting )
                 );
 
         // auto-indent: insert previous line's indents on enter
@@ -105,131 +128,4 @@ public class MainTabPane extends TabPane {
             "",
             "}"
     });
-
-    private static final String[] KEYWORDS = new String[] {
-            "abstract", "assert", "boolean", "break", "byte",
-            "case", "catch", "char", "class", "const",
-            "continue", "default", "do", "double", "else",
-            "enum", "extends", "final", "finally", "float",
-            "for", "goto", "if", "implements", "import",
-            "instanceof", "int", "interface", "long", "native",
-            "new", "package", "private", "protected", "public",
-            "return", "short", "static", "strictfp", "super",
-            "switch", "synchronized", "this", "throw", "throws",
-            "transient", "try", "void", "volatile", "while"
-    };
-
-    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
-    private static final String PAREN_PATTERN = "\\(|\\)";
-    private static final String BRACE_PATTERN = "\\{|\\}";
-    private static final String BRACKET_PATTERN = "\\[|\\]";
-    private static final String SEMICOLON_PATTERN = "\\;";
-    private static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
-    private static final String COMMENT_PATTERN = "//[^\n]*" + "|" + "/\\*(.|\\R)*?\\*/"   // for whole text processing (text blocks)
-            + "|" + "/\\*[^\\v]*" + "|" + "^\\h*\\*([^\\v]*|/)";  // for visible paragraph processing (line by line)
-
-    private static final Pattern PATTERN = Pattern.compile(
-            "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
-                    + "|(?<PAREN>" + PAREN_PATTERN + ")"
-                    + "|(?<BRACE>" + BRACE_PATTERN + ")"
-                    + "|(?<BRACKET>" + BRACKET_PATTERN + ")"
-                    + "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
-                    + "|(?<STRING>" + STRING_PATTERN + ")"
-                    + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
-    );
-
-    private StyleSpans<Collection<String>> computeHighlighting(String text) {
-        Matcher matcher = PATTERN.matcher(text);
-        int lastKwEnd = 0;
-        StyleSpansBuilder<Collection<String>> spansBuilder
-                = new StyleSpansBuilder<>();
-        while(matcher.find()) {
-            String styleClass =
-                    matcher.group("KEYWORD") != null ? "keyword" :
-                            matcher.group("PAREN") != null ? "paren" :
-                                    matcher.group("BRACE") != null ? "brace" :
-                                            matcher.group("BRACKET") != null ? "bracket" :
-                                                    matcher.group("SEMICOLON") != null ? "semicolon" :
-                                                            matcher.group("STRING") != null ? "string" :
-                                                                    matcher.group("COMMENT") != null ? "comment" :
-                                                                            null; /* never happens */ assert styleClass != null;
-            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
-            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
-            lastKwEnd = matcher.end();
-        }
-        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
-        return spansBuilder.create();
-    }
-
-    private class VisibleParagraphStyler<PS, SEG, S> implements Consumer<ListModification<? extends Paragraph<PS, SEG, S>>>
-    {
-        private final GenericStyledArea<PS, SEG, S> area;
-        private final Function<String,StyleSpans<S>> computeStyles;
-        private int prevParagraph, prevTextLength;
-
-        public VisibleParagraphStyler( GenericStyledArea<PS, SEG, S> area, Function<String,StyleSpans<S>> computeStyles )
-        {
-            this.computeStyles = computeStyles;
-            this.area = area;
-        }
-
-        @Override
-        public void accept( ListModification<? extends Paragraph<PS, SEG, S>> lm )
-        {
-            if ( lm.getAddedSize() > 0 ) Platform.runLater( () ->
-            {
-                int paragraph = Math.min( area.firstVisibleParToAllParIndex() + lm.getFrom(), area.getParagraphs().size()-1 );
-                String text = area.getText( paragraph, 0, paragraph, area.getParagraphLength( paragraph ) );
-
-                if ( paragraph != prevParagraph || text.length() != prevTextLength )
-                {
-                    if ( paragraph < area.getParagraphs().size()-1 )
-                    {
-                        int startPos = area.getAbsolutePosition( paragraph, 0 );
-                        area.setStyleSpans( startPos, computeStyles.apply( text ) );
-                    }
-                    prevTextLength = text.length();
-                    prevParagraph = paragraph;
-                }
-            });
-        }
-    }
-
-    private class DefaultContextMenu extends ContextMenu
-    {
-        private MenuItem fold, unfold, print;
-
-        public DefaultContextMenu()
-        {
-            fold = new MenuItem( "Fold selected text" );
-            fold.setOnAction( AE -> { hide(); fold(); } );
-
-            unfold = new MenuItem( "Unfold from cursor" );
-            unfold.setOnAction( AE -> { hide(); unfold(); } );
-
-            print = new MenuItem( "Print" );
-            print.setOnAction( AE -> { hide(); print(); } );
-
-            getItems().addAll( fold, unfold, print );
-        }
-
-        /**
-         * Folds multiple lines of selected text, only showing the first line and hiding the rest.
-         */
-        private void fold() {
-            ((CodeArea) getOwnerNode()).foldSelectedParagraphs();
-        }
-
-        /**
-         * Unfold the CURRENT line/paragraph if it has a fold.
-         */
-        private void unfold() {
-            CodeArea area = (CodeArea) getOwnerNode();
-            area.unfoldParagraphs( area.getCurrentParagraph() );
-        }
-
-        private void print() {
-            System.out.println( ((CodeArea) getOwnerNode()).getText() );
-        }
-    }
 }
