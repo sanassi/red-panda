12e12be7205a45284e487072453774f0b1d48d3a
Terence <terence.degroote@epita.fr>
2023-06-29 19:04:40 +0200

    feat(git):all features

--- src/main/java/com/example/demo/MainMenuBar.java
+++ src/main/java/com/example/demo/MainMenuBar.java
@@ -30,5 +30,28 @@ public class MainMenuBar extends MenuBar {
     @FXML
     public void setMenus(MainWindowController windowController) {
         windowController.addProjectFolderChooser(openProject, "Open Project");
+
+    }
+
+    @FXML
+    public void setGitMenu(MainWindowController windowController)
+    {
+        Menu gitMenu = new Menu("Git");
+        MenuItem addFile = new MenuItem("Add File");
+        MenuItem commit = new MenuItem("commit");
+        MenuItem push = new MenuItem("push");
+        MenuItem pull = new MenuItem("pull");
+
+        //git_add(addFile,"file to add");
+        MenuItem closeMenu = new MenuItem("close");
+        gitMenu.getItems().addAll(addFile, commit, push, pull ,closeMenu);
+        windowController.mainMenuBar.getMenus().add(gitMenu);
+        windowController.addGitFeatures(addFile, "add");
+        windowController.commitGitFeatures(commit);
+        windowController.pullGitFeatures(pull);
+        windowController.pushGitFeatures(push);
+
+
+
     }
 }
