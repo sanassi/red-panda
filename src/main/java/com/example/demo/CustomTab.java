3d7ac66e526e26a6004c5a3a071edc7621ed97c4
Sanassi Cissé <sanassi-mory.cisse@epita.fr>
2023-07-03 11:26:32 +0200

    feat: started working to replacing Tab with CustomTab, to store more data about the curent tab (ex: if it was stored before, etc.)
    Signed-off-by: Sanassi Cissé <sanassi-mory.cisse@epita.fr>

---
+++ src/main/java/com/example/demo/CustomTab.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import com.example.demo.myide.domain.entity.Node;
+import javafx.scene.control.Tab;
+
+public class CustomTab extends Tab {
+    public Node nodeData;
+}
