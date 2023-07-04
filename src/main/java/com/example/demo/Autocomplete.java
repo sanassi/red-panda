86ec2c37f7a4eaa59d2d1fd00e9c4a16e854eb98
Sanassi Cissé <sanassi-mory.cisse@epita.fr>
2023-07-05 00:48:26 +0200

    fix: modified whitespace check, used to add invalid characters in regex, which throws an error
    Signed-off-by: Sanassi Cissé <sanassi-mory.cisse@epita.fr>

--- src/main/java/com/example/demo/Autocomplete.java
+++ src/main/java/com/example/demo/Autocomplete.java
@@ -11,23 +11,31 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Autocomplete {
+    /**
+     * Utility function to check if character is not whitespace
+     * @param c
+     * @return Whether c is not a whitespace character
+     */
+    public static Boolean isNotWhite(Character c) {
+        Pattern pattern = Pattern.compile("[a-zA-Z0-9_]+");
+        return pattern.matcher(String.valueOf(c)).matches();
+    }
+
+    final static String[] keywords = new String[] {
+            "abstract", "assert", "boolean", "break", "byte",
+            "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else",
+            "enum", "extends", "final", "finally", "float",
+            "for", "goto", "if", "implements", "import",
+            "instanceof", "int", "interface", "long", "native",
+            "new", "package", "private", "protected", "public",
+            "return", "short", "static", "strictfp", "super",
+            "switch", "synchronized", "this", "throw", "throws",
+            "transient", "try", "void", "volatile", "while"
+    };
 
     public static void setListener(CodeArea codeArea) {
-        final String[] keywords = new String[] {
-                "abstract", "assert", "boolean", "break", "byte",
-                "case", "catch", "char", "class", "const",
-                "continue", "default", "do", "double", "else",
-                "enum", "extends", "final", "finally", "float",
-                "for", "goto", "if", "implements", "import",
-                "instanceof", "int", "interface", "long", "native",
-                "new", "package", "private", "protected", "public",
-                "return", "short", "static", "strictfp", "super",
-                "switch", "synchronized", "this", "throw", "throws",
-                "transient", "try", "void", "volatile", "while"
-        };
-
         ContextMenu suggestions = new ContextMenu();
-        //suggestions.hide();
 
         List<String> words = Arrays.stream(keywords).toList();
 
@@ -40,14 +48,14 @@ public class Autocomplete {
             int index ;
 
             for (index = text.length() - 1;
-                 index >= 0 && ! Character.isWhitespace(text.charAt(index));
+                 index >= 0 && /*! Character.isWhitespace*/isNotWhite(text.charAt(index));
                  index--);
 
             int startIndex = Math.max(index, 0);
             String prefix = text.substring(index+1);
 
             for (index = newPosition;
-                 index < codeArea.getLength() && ! Character.isWhitespace(codeArea.getText().charAt(index));
+                 index < codeArea.getLength() && /*! Character.isWhitespace*/isNotWhite(codeArea.getText().charAt(index));
                  index++);
 
             String suffix = codeArea.getText().substring(newPosition, index);
@@ -61,15 +69,13 @@ public class Autocomplete {
 
             Pattern pattern = Pattern.compile(prefix+".+"+suffix,
                     Pattern.CASE_INSENSITIVE);
-
             String finalPrefix = prefix;
             suggestions.getItems().setAll(
                     words.stream().filter(word -> pattern.matcher(word).matches())
                             .map(word -> {
                                 MenuItem item = new MenuItem(word);
-                                item.setOnAction(click -> {
-                                    codeArea.replaceText(codeArea.getCaretPosition() - finalPrefix.length(), codeArea.getCaretPosition(), word);
-                                });
+                                item.setOnAction(click -> codeArea.replaceText(codeArea.getCaretPosition() - finalPrefix.length(),
+                                        codeArea.getCaretPosition(), word));
                                 return item;
                             })
                             .limit(100)
